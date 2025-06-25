4f90c83c1902f96172477af5abfbabf2d1bdca1e
arifdamri <arifdamri45@gmail.com>
2025-06-25 23:05:17 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function showWish() {
+  alert("💌 Dear Lovie,\n\nWishing you the happiest birthday! You bring so much light into my life. Keep smiling, keep shining. 🎉🎂💖\n\nLove always,\nArif 💕");
+}
